fa3960fa5d6ee612ed9b6d3402be58fc35f65990
xtianmpimbaza <xtianm4@gmail.com>
2024-03-20 11:47:52 +0300

    structured

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -145,43 +145,6 @@ if (strlen($_SESSION['alogin']) == 0) {
             </div>
 
 
-<!--            <div class="row">-->
-<!---->
-<!--                <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">-->
-<!--                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel">-->
-<!---->
-<!--                        <div class="carousel-inner">-->
-<!--                            <div class="item active">-->
-<!---->
-<!--                                <img src="assets/img/1.jpg" alt=""/>-->
-<!---->
-<!--                            </div>-->
-<!--                            <div class="item">-->
-<!--                                <img src="assets/img/2.jpg" alt=""/>-->
-<!---->
-<!--                            </div>-->
-<!--                            <div class="item">-->
-<!--                                <img src="assets/img/3.jpg" alt=""/>-->
-<!---->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        INDICATORS-->
-<!--                        <ol class="carousel-indicators">-->
-<!--                            <li data-target="#carousel-example" data-slide-to="0" class="active"></li>-->
-<!--                            <li data-target="#carousel-example" data-slide-to="1"></li>-->
-<!--                            <li data-target="#carousel-example" data-slide-to="2"></li>-->
-<!--                        </ol>-->
-<!--                        PREVIUS-NEXT BUTTONS-->
-<!--                        <a class="left carousel-control" href="#carousel-example" data-slide="prev">-->
-<!--                            <span class="glyphicon glyphicon-chevron-left"></span>-->
-<!--                        </a>-->
-<!--                        <a class="right carousel-control" href="#carousel-example" data-slide="next">-->
-<!--                            <span class="glyphicon glyphicon-chevron-right"></span>-->
-<!--                        </a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-
         </div>
     </div>
     <!-- CONTENT-WRAPPER SECTION END-->
