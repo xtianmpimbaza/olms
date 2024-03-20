d92b495edba037d77c0281795639fcd927c1a263
xtianmpimbaza <xtianm4@gmail.com>
2024-03-20 11:39:08 +0300

    simplified

--- signup.php
+++ signup.php
@@ -4,38 +4,32 @@ include('includes/config.php');
 error_reporting(0);
 if (isset($_POST['signup'])) {
 //code for captach verification
-    if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"] == '') {
-        echo "<script>alert('Incorrect verification code');</script>";
-    } else {
+
 //Code for student ID
-        $count_my_page = ("studentid.txt");
-        $hits = file($count_my_page);
-        $hits[0]++;
-        $fp = fopen($count_my_page, "w");
-        fputs($fp, "$hits[0]");
-        fclose($fp);
-        $StudentId = $hits[0];
-        $fname = $_POST['fullanme'];
-        $mobileno = $_POST['mobileno'];
-        $email = $_POST['email'];
-        $password = md5($_POST['password']);
-        $status = 1;
-        $sql = "INSERT INTO  tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES(:StudentId,:fname,:mobileno,:email,:password,:status)";
-        $query = $dbh->prepare($sql);
-        $query->bindParam(':StudentId', $StudentId, PDO::PARAM_STR);
-        $query->bindParam(':fname', $fname, PDO::PARAM_STR);
-        $query->bindParam(':mobileno', $mobileno, PDO::PARAM_STR);
-        $query->bindParam(':email', $email, PDO::PARAM_STR);
-        $query->bindParam(':password', $password, PDO::PARAM_STR);
-        $query->bindParam(':status', $status, PDO::PARAM_STR);
-        $query->execute();
-        $lastInsertId = $dbh->lastInsertId();
-        if ($lastInsertId) {
-            echo '<script>alert("Your Registration successfull and your student id is  "+"' . $StudentId . '")</script>';
-        } else {
-            echo "<script>alert('Something went wrong. Please try again');</script>";
-        }
+
+    $StudentId = 'SID013';
+    $fname = $_POST['fullanme'];
+    $mobileno = $_POST['mobileno'];
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+    $status = 1;
+    $sql = "INSERT INTO  tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES(:StudentId,:fname,:mobileno,:email,:password,:status)";
+
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':StudentId', $StudentId, PDO::PARAM_STR);
+    $query->bindParam(':fname', $fname, PDO::PARAM_STR);
+    $query->bindParam(':mobileno', $mobileno, PDO::PARAM_STR);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->bindParam(':status', $status, PDO::PARAM_STR);
+    $query->execute();
+    $lastInsertId = $dbh->lastInsertId();
+    if ($lastInsertId) {
+        echo '<script>alert("Your Registration successfull and your student id is  "+"' . $StudentId . '")</script>';
+    } else {
+        echo "<script>alert('Something went wrong. Please try again');</script>";
     }
+
 }
 ?>
 
@@ -138,12 +132,9 @@ if (isset($_POST['signup'])) {
                                 <input class="form-control" type="password" name="confirmpassword" autocomplete="off"
                                        required/>
                             </div>
-                            <div class="form-group">
-                                <label>Verification code : </label>
-                                <input type="text" name="vercode" maxlength="5" autocomplete="off" required
-                                       style="width: 150px; height: 25px;"/>&nbsp;<img src="captcha.php">
-                            </div>
-                            <button type="submit" name="signup" class="btn btn-primary" id="submit">Register Now</button>
+
+                            <button type="submit" name="signup" class="btn btn-primary" id="submit">Register Now
+                            </button>
 
                         </form>
                     </div>
